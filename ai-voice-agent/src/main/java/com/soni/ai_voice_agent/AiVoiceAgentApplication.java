7d9ffd67963b36f1d23f5629d8d03ab77d940706
Vishal Soni <vishalsonioff@gmail.com>
2026-06-23 12:21:59 +0530

    Intial project setup

---
+++ ai-voice-agent/src/main/java/com/soni/ai_voice_agent/AiVoiceAgentApplication.java
@@ -0,0 +1,13 @@
+package com.soni.ai_voice_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiVoiceAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiVoiceAgentApplication.class, args);
+	}
+
+}
